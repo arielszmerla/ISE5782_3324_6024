eb7abfa090723b95d29039d2b3f31774c0f3464e
Ariel Szmerla <aszmerla@g.jct.ac.il>
2022-04-10 22:02:18 +0300

    corrected rendering shlav 6

--- src/renderer/RayTracerBasic.java
+++ src/renderer/RayTracerBasic.java
@@ -1,12 +1,14 @@
 package renderer;
 
-import primitives.Color;
-import primitives.Ray;
+import lighting.LightSource;
+import primitives.*;
 import scene.Scene;
 import geometries.Intersectable.GeoPoint;
 
 
 import java.util.List;
+
+import static primitives.Util.*;
 
 public class RayTracerBasic extends RayTracer {
     public RayTracerBasic(Scene scene) {
@@ -31,4 +33,22 @@ public class RayTracerBasic extends RayTracer {
     private Color calcColor(GeoPoint geoPoint, Ray ray) {
         return geoPoint._geometry.getEmission();
     }
+//    private Color calcLocalEffects(GeoPoint intersection, Ray ray) {
+//        Vector v = ray.getDir(); Vector n = intersection._geometry.getNormal();
+//        double nv= alignZero(n.dotProduct(v);
+//        if (nv== 0) return Color.BLACK;
+//        int nShininess= intersection._geometry.getnShininess();
+//        double kd= intersection._geometry.getKd(), ks= intersection._geometry.getKs();
+//        Color color= Color.BLACK;
+//        for (LightSource lightSource: scene.lights) {
+//            Vector l = lightSource.getL(intersection._point);
+//            double nl= alignZero(n.dotProduct(l);
+//            if (nl* nv> 0) { // sign(nl) == sing(nv)
+//                Color lightIntensity= lightSource.getIntensity(intersection._point);
+//                color = color.add(calcDiffusive(kd, l, n, lightIntensity),
+//                        calcSpecular(ks, l, n, v, nShininess, lightIntensity));
+//            }
+//        }
+//        return color;
+//    }
 }
